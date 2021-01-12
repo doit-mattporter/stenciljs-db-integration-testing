54a2cd8588605b70b57f50676c828b2b8756d830
doit-mattporter <matt.porter@doit-intl.com>
2021-01-11 18:48:52 -0800

    Fixed private IP for Cloud SQL issue. Split up GCE bootstrapping script.

--- bootstrap/project_setup.sh
+++ bootstrap/project_setup.sh
@@ -23,12 +23,6 @@ wait
 # Create a Secret Manager secret holding the Cloud SQL admin password
 echo $MYSQL_ROOT_PWD | gcloud secrets create wfe-mysql-root --data-file=-
 echo $MYSQL_CONTACT_USER_PWD | gcloud secrets create wfe-mysql-contact-user --data-file=-
-
-# Run queries for MySQL from Cloud Shell through Cloud SQL Proxy
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
-chmod +x cloud_sql_proxy
-nohup ./cloud_sql_proxy -instances=$PROJECT_ID:$REGION:wfe-mysql=tcp:3306 &
-
 # Create MySQL server hosted on Cloud SQL with a private IP
 gcloud compute addresses create google-managed-services-default \
     --global \
@@ -39,15 +33,21 @@ gcloud compute addresses create google-managed-services-default \
 gcloud services vpc-peerings connect \
     --service=servicenetworking.googleapis.com \
     --ranges=google-managed-services-default \
-    --network="projects/$PROJECT_ID/global/networks/default" \
+    --network="default" \
     --project=$PROJECT_ID
 
-gcloud beta sql instances create wfe-mysql6 \
+gcloud beta sql instances create wfe-mysql \
     --database-version=MYSQL_8_0 \
     --tier=db-n1-standard-1 \
     --zone=$ZONE \
     --network="projects/$PROJECT_ID/global/networks/default" \
     --root-password=$MYSQL_ROOT_PWD
+
+# Run queries for MySQL from Cloud Shell through Cloud SQL Proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+nohup ./cloud_sql_proxy -instances=$PROJECT_ID:$REGION:wfe-mysql=tcp:3306 &
+sleep 3
 
 # Initialize MySQL VM with empty contactdb database, Contacts table, and contact_form_write_user user
 gcloud sql --project=$PROJECT_ID databases create contactdb --instance=wfe-mysql
@@ -82,45 +82,45 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
     --role="roles/cloudsql.client"
 
 # Create firewall rules for the StencilJS demo VM
-nohup gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-http \
+gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-http \
     --direction=INGRESS \
     --priority=1000 \
     --network=default \
     --action=ALLOW \
     --rules=tcp:80 \
     --source-ranges=0.0.0.0/0 \
-    --target-tags=http-server &
+    --target-tags=http-server
 
-nohup gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-https \
+gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-https \
     --direction=INGRESS \
     --priority=1000 \
     --network=default \
     --action=ALLOW \
     --rules=tcp:443 \
     --source-ranges=0.0.0.0/0 \
-    --target-tags=https-server &
+    --target-tags=https-server
 
-nohup gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-mysql \
+gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-mysql \
     --direction=EGRESS \
     --priority=1000 \
     --network=default \
     --action=ALLOW \
     --rules=tcp:3306 \
-    --target-tags=mysql-client &
-
-wait
+    --target-tags=mysql-client
 
 # Create StencilJS demo VM
 # Bootstrap code used in VM metadata:
-# gsutil cp gs://$CODE_BUCKET-code-bucket/stenciljs_demo.tar.gz .
+# gsutil cp gs://$CODE_BUCKET/stenciljs_demo.tar.gz .
 # tar -C /opt/ -zxf stenciljs_demo.tar.gz
-# chmod +x /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap.sh
+# chmod +x /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part*.sh
+# /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part1.sh
+# /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part2.sh
 gcloud compute --project=$PROJECT_ID instances create nodejs-stenciljs \
     --zone=$ZONE \
     --machine-type=e2-medium \
     --subnet=default \
     --network-tier=PREMIUM \
-    --metadata=startup-script=gsutil\ cp\ gs://$CODE_BUCKET-code-bucket/stenciljs_demo.tar.gz\ .$'\n'tar\ -C\ /opt/\ -zxf\ stenciljs_demo.tar.gz$'\n'chmod\ \+x\ /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap.sh \
+    --metadata=startup-script=gsutil\ cp\ gs://\$CODE_BUCKET/stenciljs_demo.tar.gz\ .$'\n'tar\ -C\ /opt/\ -zxf\ stenciljs_demo.tar.gz$'\n'chmod\ \+x\ /opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part\*.sh$'\n'/opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part1.sh$'\n'/opt/stenciljs-db-integration-testing/bootstrap/nodejs_bootstrap_part2.sh \
     --maintenance-policy=MIGRATE \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
     --service-account=stenciljs-demo-sa@$PROJECT_ID.iam.gserviceaccount.com \
